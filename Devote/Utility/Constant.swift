897700a56fd97ce50032645da9aaf593d229b321
Mehul Oswal <mehuloswal21@gmail.com>
2022-08-04 00:04:40 +0530

    Restructured

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Mehul Oswal on 03/08/22.
+//
+
+import SwiftUI
+
+//MARK: - FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+//MARK: -UI
+
+
+//MARK: -UX 
